87049d5c5aaa764f9432711e5c5fe151cc5f8d29
TaniaShmigel <tatjanka222@gmail.com>
2023-03-05 19:39:06 +0200

    added task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,14 +8,25 @@ import ListContact from './ListContact';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts !== null) {
+      const parseContacts = JSON.parse(savedContacts);
+      this.setState({ contacts: parseContacts });
+      return;
+    }
+    this.setState({ contacts: [] });
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   handleSubmit = (values, { resetForm }) => {
     const newContact = values;
